b87e8cf5e02840b847e7ef69026625df727cdcb1
Vixting <vixting@gmail.com>
2026-08-24 13:53:14 +0100

    corrososive

--- Content.Shared/_RMC14/Chemistry/Effects/Negative/Corrosive.cs
+++ Content.Shared/_RMC14/Chemistry/Effects/Negative/Corrosive.cs
@@ -1,16 +1,26 @@
+using Content.Shared._RMC14.Chemistry.Buildup;
 using Content.Shared._RMC14.Damage;
+using Content.Shared._RMC14.Synth;
+using Content.Shared._RMC14.Xenonids;
 using Content.Shared.Botany.Components;
+using Content.Shared.Chemistry;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.EntityEffects;
 using Content.Shared.FixedPoint;
+using Content.Shared.Humanoid;
+using Content.Shared.Inventory;
+using Content.Shared.Item;
+using Content.Shared.Mobs.Components;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
 
 namespace Content.Shared._RMC14.Chemistry.Effects.Negative;
 
 public sealed partial class Corrosive : RMCChemicalEffect
 {
     private static readonly ProtoId<DamageTypePrototype> CausticType = "Caustic";
+    private static readonly ProtoId<DamageGroupPrototype> BruteGroup = "Brute";
 
     protected override string ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
     {
@@ -21,9 +31,95 @@ public sealed partial class Corrosive : RMCChemicalEffect
 
     protected override void Tick(DamageableSystem damageable, FixedPoint2 potency, EntityEffectReagentArgs args)
     {
+        if (args.Method == ReactionMethod.Touch)
+        {
+            HandleTouch(damageable, args);
+            return;
+        }
+
         var damage = new DamageSpecifier();
         damage.DamageDict[CausticType] = potency;
         damageable.TryChangeDamage(args.TargetEntity, damage, true, interruptsDoAfters: false);
+    }
+
+    private void HandleTouch(DamageableSystem damageable, EntityEffectReagentArgs args)
+    {
+        var entities = args.EntityManager;
+        var target = args.TargetEntity;
+
+        if (!entities.HasComponent<MobStateComponent>(target))
+        {
+            HandleTouchItem(args);
+            return;
+        }
+
+        // meltprob = potency * POTENCY_MULTIPLIER_HIGH (3), expressed as a 0-1 chance here
+        var meltChance = (float) ActualPotency * 0.03f;
+        var isHuman = entities.HasComponent<HumanoidAppearanceComponent>(target) && !entities.HasComponent<SynthComponent>(target);
+        if (isHuman)
+        {
+            var inventory = entities.System<InventorySystem>();
+            var random = IoCManager.Resolve<IRobustRandom>();
+
+            if (inventory.TryGetSlotEntity(target, "head", out var head))
+            {
+                if (random.Prob(meltChance))
+                {
+                    inventory.TryUnequip(target, "head", force: true);
+                    entities.QueueDeleteEntity(head);
+                }
+
+                return;
+            }
+
+            if (inventory.TryGetSlotEntity(target, "mask", out var mask))
+            {
+                if (random.Prob(meltChance))
+                {
+                    inventory.TryUnequip(target, "mask", force: true);
+                    entities.QueueDeleteEntity(mask);
+                }
+
+                return;
+            }
+
+            if (inventory.TryGetSlotEntity(target, "eyes", out var eyes))
+            {
+                if (random.Prob(meltChance))
+                {
+                    inventory.TryUnequip(target, "eyes", force: true);
+                    entities.QueueDeleteEntity(eyes);
+                }
+
+                return;
+            }
+        }
+
+        // TODO RMC14: head burn damage instead of generic brute
+        var volume = (float) args.Quantity;
+        var rmcDamageable = entities.System<SharedRMCDamageableSystem>();
+        var damage = rmcDamageable.DistributeDamageCached(target, BruteGroup, MathF.Min(6f, volume));
+        damageable.TryChangeDamage(target, damage, true, interruptsDoAfters: false);
+
+        if (entities.HasComponent<XenoComponent>(target) && ActualPotency > 2f)
+        {
+            var toxicBuildup = entities.System<RMCToxicBuildupSystem>();
+            var magnitude = ActualPotency * volume * 0.25f;
+            toxicBuildup.AddBuildup(target, magnitude, 1f / 3f, 75f);
+        }
+    }
+
+    private void HandleTouchItem(EntityEffectReagentArgs args)
+    {
+        var entities = args.EntityManager;
+        var target = args.TargetEntity;
+
+        if (!entities.HasComponent<ItemComponent>(target))
+            return;
+
+        var random = IoCManager.Resolve<IRobustRandom>();
+        if (random.Prob((float) ActualPotency * 0.1f))
+            entities.QueueDeleteEntity(target);
     }
 
     protected override void TickOverdose(DamageableSystem damageable, FixedPoint2 potency, EntityEffectReagentArgs args)
