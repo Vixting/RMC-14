8a7026182fcf1af0183f0101fbfc8f468eac89b4
Vixting <vixting@gmail.com>
2026-09-11 09:28:20 +0100

    atomized to other branch

--- Content.Shared/_RMC14/Chemistry/SmartFridge/SharedRMCSmartFridgeSystem.cs
+++ Content.Shared/_RMC14/Chemistry/SmartFridge/SharedRMCSmartFridgeSystem.cs
@@ -1,12 +1,7 @@
-﻿using System.Linq;
-using Content.Shared.Chemistry.Reagent;
-using Content.Shared.Chemistry.EntitySystems;
-using Content.Shared.FixedPoint;
-using Content.Shared.Hands.EntitySystems;
+﻿using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 namespace Content.Shared._RMC14.Chemistry.SmartFridge;
@@ -16,7 +11,6 @@ public abstract class SharedRMCSmartFridgeSystem : EntitySystem
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
-    [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
 
     private readonly HashSet<Entity<RMCSmartFridgeComponent>> _smartFridges = new();
 
@@ -51,162 +45,6 @@ public abstract class SharedRMCSmartFridgeSystem : EntitySystem
         var container = _container.EnsureContainer<Container>(fridge.Value, fridge.Value.Comp.ContainerId);
         _container.Insert(transfer, container);
         Dirty(fridge.Value);
-    }
-
-    public bool TryDrainStock(EntityCoordinates coords, float range, ProtoId<ReagentPrototype> reagent, FixedPoint2 amount, out FixedPoint2 drained, IReadOnlySet<EntityUid>? exclude = null)
-    {
-        drained = FixedPoint2.Zero;
-
-        _smartFridges.Clear();
-        _entityLookup.GetEntitiesInRange(coords, range, _smartFridges);
-        if (!_smartFridges.TryFirstOrNull(out var fridge))
-            return false;
-
-        if (!_container.TryGetContainer(fridge.Value, fridge.Value.Comp.ContainerId, out var container))
-            return false;
-
-        foreach (var contained in container.ContainedEntities.ToArray())
-        {
-            if (drained >= amount)
-                break;
-
-            if (exclude != null && exclude.Contains(contained))
-                continue;
-
-            foreach (var (_, solnEnt) in _solution.EnumerateSolutions((contained, null)).ToArray())
-            {
-                var solution = solnEnt.Comp.Solution;
-                var have = solution.GetReagentQuantity(new ReagentId(reagent, null));
-                if (have <= FixedPoint2.Zero)
-                    continue;
-
-                var take = FixedPoint2.Min(have, amount - drained);
-                if (take <= FixedPoint2.Zero)
-                    continue;
-
-                _solution.RemoveReagent(solnEnt, reagent, take);
-                drained += take;
-
-                if (solution.Volume <= FixedPoint2.Zero &&
-                    TryComp(contained, out RMCSmartFridgeInsertableComponent? insertable) &&
-                    insertable.Category == "Bottle")
-                {
-                    _container.Remove(contained, container);
-                    QueueDel(contained);
-                }
-
-                if (drained >= amount)
-                    break;
-            }
-        }
-
-        if (drained > FixedPoint2.Zero)
-            Dirty(fridge.Value);
-
-        return drained > FixedPoint2.Zero;
-    }
-
-    public bool TryGetEmptyContainer(EntityCoordinates coords, float range, FixedPoint2 neededVolume, out EntityUid container)
-    {
-        container = default;
-
-        _smartFridges.Clear();
-        _entityLookup.GetEntitiesInRange(coords, range, _smartFridges);
-        if (!_smartFridges.TryFirstOrNull(out var fridge))
-            return false;
-
-        if (!_container.TryGetContainer(fridge.Value, fridge.Value.Comp.ContainerId, out var fridgeContainer))
-            return false;
-
-        var bestFit = FixedPoint2.Zero;
-        var bestOverall = FixedPoint2.Zero;
-        EntityUid? bestFitEnt = null;
-        EntityUid? bestOverallEnt = null;
-
-        foreach (var contained in fridgeContainer.ContainedEntities)
-        {
-            foreach (var (_, solnEnt) in _solution.EnumerateSolutions((contained, null)))
-            {
-                var solution = solnEnt.Comp.Solution;
-                if (solution.Volume > FixedPoint2.Zero)
-                    continue;
-
-                if (solution.MaxVolume > bestOverall)
-                {
-                    bestOverall = solution.MaxVolume;
-                    bestOverallEnt = contained;
-                }
-
-                if (solution.MaxVolume >= neededVolume && (bestFitEnt == null || solution.MaxVolume < bestFit))
-                {
-                    bestFit = solution.MaxVolume;
-                    bestFitEnt = contained;
-                }
-            }
-        }
-
-        container = bestFitEnt ?? bestOverallEnt ?? default;
-        return container != default;
-    }
-
-    public bool TryGetEmptyContainerByPrototype(EntityCoordinates coords, float range, EntProtoId prototype, out EntityUid container)
-    {
-        container = default;
-
-        _smartFridges.Clear();
-        _entityLookup.GetEntitiesInRange(coords, range, _smartFridges);
-        if (!_smartFridges.TryFirstOrNull(out var fridge))
-            return false;
-
-        if (!_container.TryGetContainer(fridge.Value, fridge.Value.Comp.ContainerId, out var fridgeContainer))
-            return false;
-
-        foreach (var contained in fridgeContainer.ContainedEntities)
-        {
-            if (MetaData(contained).EntityPrototype?.ID != prototype.Id)
-                continue;
-
-            foreach (var (_, solnEnt) in _solution.EnumerateSolutions((contained, null)))
-            {
-                if (solnEnt.Comp.Solution.Volume <= FixedPoint2.Zero)
-                {
-                    container = contained;
-                    return true;
-                }
-
-                break;
-            }
-        }
-
-        return false;
-    }
-
-    public int GetEmptyContainerCount(EntityCoordinates coords, float range, EntProtoId prototype)
-    {
-        _smartFridges.Clear();
-        _entityLookup.GetEntitiesInRange(coords, range, _smartFridges);
-        if (!_smartFridges.TryFirstOrNull(out var fridge))
-            return 0;
-
-        if (!_container.TryGetContainer(fridge.Value, fridge.Value.Comp.ContainerId, out var fridgeContainer))
-            return 0;
-
-        var count = 0;
-        foreach (var contained in fridgeContainer.ContainedEntities)
-        {
-            if (MetaData(contained).EntityPrototype?.ID != prototype.Id)
-                continue;
-
-            foreach (var (_, solnEnt) in _solution.EnumerateSolutions((contained, null)))
-            {
-                if (solnEnt.Comp.Solution.Volume <= FixedPoint2.Zero)
-                    count++;
-
-                break;
-            }
-        }
-
-        return count;
     }
 
     private void OnVend(Entity<RMCSmartFridgeComponent> ent, ref RMCSmartFridgeVendMsg args)
